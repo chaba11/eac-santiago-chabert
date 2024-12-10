c385729d73da9e957be90d1bb4c608085c753a11
Santiago Chabert <santiago.chabert@xmartlabs.com>
2024-12-10 09:38:38 -0300

    Commit inicial

--- src/components/Equipo.jsx
+++ src/components/Equipo.jsx
@@ -1,5 +1,12 @@
-function Equipo() {
-  return <h1>Hola Mundo</h1>;
+function Equipo({ nombreEquipo, year, nombreEstadio, urlEquipo }) {
+  return (
+    <div>
+      <p>Nombre equipo: {nombreEquipo || "Desconocido"}</p>
+      <p>Año de fundación: {year || "Desconocido"}</p>
+      <p>Nombre del Estadio: {nombreEstadio || "Desconocido"}</p>
+      <img src={urlEquipo}></img>
+    </div>
+  );
 }
 
 export { Equipo };
